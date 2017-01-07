eb1456ee2b8f4e0a90ca73d6c611ad755eb28a67
lee <ljw3728@gmail.com>
2017-01-07 16:22:31 +0900

    .gitignore is working, added 10th row of api

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,7 +1,7 @@
 class BusesController < ApplicationController
   def index
     response = JSON.parse(HTTParty.get "http://scard.skku.edu/Symtra_Bus/BusLocationJson.asp")
-    for n in [1,2,3,4,5,6,7,8,9]
+    for n in [1,2,3,4,5,6,7,8,9,10]
       @json = response[n-1],
       instance_variable_set("@json#{n}", response[n-1]),
       instance_variable_set("@sequence#{n}", response[n-1]["Sequence"]),
