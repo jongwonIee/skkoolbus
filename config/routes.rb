611e7fadb9862d32bdea96b82320778b5caf91e6
lee <ljw3728@gmail.com>
2017-01-07 17:32:58 +0900

    therubyracer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'buses#index'
   get 'buses/index'
+  get 'schedule' => 'buses#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
