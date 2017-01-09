814d99c5edbdad35e385d96d64e933bc5f978482
lee <ljw3728@gmail.com>
2017-01-09 18:49:53 +0900

    exception handling for bus overlap

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,15 +1,28 @@
 class BusesController < ApplicationController
   def index
     response = JSON.parse(HTTParty.get "http://scard.skku.edu/Symtra_Bus/BusLocationJson.asp")
-    for n in [1,2,3,4,5,6,7,8,9,10]
-      @json = response[n-1],
-      instance_variable_set("@json#{n}", response[n-1]),
-      instance_variable_set("@sequence#{n}", response[n-1]["Sequence"]),
-      instance_variable_set("@stationName#{n}", response[n-1]["StationName"]),
-      instance_variable_set("@eventDate#{n}", response[n-1]["EventDate"]),
-      instance_variable_set("@kind#{n}", response[n-1]["Kind"]),
-      instance_variable_set("@useTime#{n}", response[n-1]["usetime"]),
-      instance_variable_set("@carNumber#{n}", response[n-1]["CarNumber"])
+    if response[10] != nil
+      for n in [1,2,3,4,5,6,7,8,9,10,11]
+        @json = response[n-1],
+            instance_variable_set("@json#{n}", response[n-1]),
+            instance_variable_set("@sequence#{n}", response[n-1]["Sequence"]),
+            instance_variable_set("@stationName#{n}", response[n-1]["StationName"]),
+            instance_variable_set("@eventDate#{n}", response[n-1]["EventDate"]),
+            instance_variable_set("@kind#{n}", response[n-1]["Kind"]),
+            instance_variable_set("@useTime#{n}", response[n-1]["usetime"]),
+            instance_variable_set("@carNumber#{n}", response[n-1]["CarNumber"])
+      end
+    else
+      for n in [1,2,3,4,5,6,7,8,9,10]
+        @json = response[n-1],
+        instance_variable_set("@json#{n}", response[n-1]),
+        instance_variable_set("@sequence#{n}", response[n-1]["Sequence"]),
+        instance_variable_set("@stationName#{n}", response[n-1]["StationName"]),
+        instance_variable_set("@eventDate#{n}", response[n-1]["EventDate"]),
+        instance_variable_set("@kind#{n}", response[n-1]["Kind"]),
+        instance_variable_set("@useTime#{n}", response[n-1]["usetime"]),
+        instance_variable_set("@carNumber#{n}", response[n-1]["CarNumber"])
+      end
     end
   end
 end
