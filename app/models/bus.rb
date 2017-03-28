7537ff669a274d64d7a44a34a20078c06c5f6f78
jongwonIee <ljw3728@gmail.com>
2017-03-29 02:59:51 +0900

    time calculation logic built

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -39,4 +39,24 @@ class Bus < ApplicationRecord
     end
     return result
   end
+
+  def self.time(n)
+    @array = [
+        ((Bus.find(1).s1 + Bus.find(2).s1 + Bus.find(3).s1) / 3),
+        ((Bus.find(1).s2 + Bus.find(2).s2 + Bus.find(3).s2) / 3),
+        ((Bus.find(1).s3 + Bus.find(2).s3 + Bus.find(3).s3) / 3),
+        ((Bus.find(1).s4 + Bus.find(2).s4 + Bus.find(3).s4) / 3),
+        ((Bus.find(1).s5 + Bus.find(2).s5 + Bus.find(3).s5) / 3),
+        ((Bus.find(1).s6 + Bus.find(2).s6 + Bus.find(3).s6) / 3),
+        ((Bus.find(1).s7 + Bus.find(2).s7 + Bus.find(3).s7) / 3),
+        ((Bus.find(1).s8 + Bus.find(2).s8 + Bus.find(3).s8) / 3),
+        ((Bus.find(1).s9 + Bus.find(2).s9 + Bus.find(3).s9) / 3),
+        ((Bus.find(1).s10 + Bus.find(2).s10 + Bus.find(3).s10) / 3)
+    ]
+    result = 0
+    (n..10).each do |i|
+      result += @array[i-1]
+    end
+    return result
+  end
 end
