ff3ed52e3b7f6ff554b7eed289dbe1cf22d57533
ljw3728@gmail.com <ljw3728@gmail.com>
2020-02-05 04:13:22 -0500

    mig is clean

--- db/migrate/20200205085521_drop_messages_table.rb
+++
@@ -1,8 +0,0 @@
-class DropMessagesTable < ActiveRecord::Migration[5.0]
-  def up
-    drop_table :messages
-  end
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200205085621) do
+ActiveRecord::Schema.define(version: 20200205090311) do
 
   create_table "buses", force: :cascade do |t|
     t.integer  "s1"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20200205085621) do
     t.integer  "time_taken"
     t.integer  "difference"
     t.integer  "sequence"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "lang"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
