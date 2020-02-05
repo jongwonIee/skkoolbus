d29d704426002b73fc3bacc6153b15ec203d74a3
ljw3728@gmail.com <ljw3728@gmail.com>
2020-02-05 03:23:13 -0500

    update seed3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# [
+#     [1,1,1,1,1,1,1,1,1,1],
+#     [1,1,1,1,1,1,1,1,1,1],
+#     [1,1,1,1,1,1,1,1,1,1],
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-[
-    [1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1],
-    [1,1,1,1,1,1,1,1,1,1],
-
-].each do |x|
-  Bus.create(s1: x[0], s2: x[1], s3: x[2], s4: x[3], s5: x[4], s6: x[5], s7: x[6], s8: x[7], s9: x[8], s10: x[9])
-end
+# ].each do |x|
+#   Bus.create(s1: x[0], s2: x[1], s3: x[2], s4: x[3], s5: x[4], s6: x[5], s7: x[6], s8: x[7], s9: x[8], s10: x[9])
+# end
 
 puts "Inserting message data.."
 File.read("db/seed_data/msg.csv").split("\n").each do |line|
@@ -24,7 +17,7 @@ File.read("db/seed_data/msg.csv").split("\n").each do |line|
   )
 end
 
-puts "Inserting message data.."
+puts "Inserting en_message data.."
 File.read("db/seed_data/msg_en.csv").split("\n").each do |line|
   data = line.strip.split("\t")
   Message.create(
