d94b7e6d1cb5a2cbd523df30c9d8ae6dc679637e
EC2 Default User <ec2-user@ip-172-31-20-2.ap-northeast-2.compute.internal>
2020-02-05 09:08:21 +0000

    fix migrate

--- db/schema.rb
+++ db/schema.rb
@@ -37,13 +37,6 @@ ActiveRecord::Schema.define(version: 20200205085621) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "lang"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "predicts", force: :cascade do |t|
     t.text     "stations"
     t.datetime "created_at", null: false
