dfdd98795f6da67fe56758dfeff30eb8d6b1fe4c
ljw3728@gmail.com <ljw3728@gmail.com>
2020-02-05 03:38:36 -0500

    adding some more messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200205015854) do
+ActiveRecord::Schema.define(version: 20200205083332) do
 
   create_table "buses", force: :cascade do |t|
     t.integer  "s1"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Bus.create(s1: x[0], s2: x[1], s3: x[2], s4: x[3], s5: x[4], s6: x[5], s7: x[6], s8: x[7], s9: x[8], s10: x[9])
 # end
 
+Message.destroy_all
+
 puts "Inserting message data.."
 File.read("db/seed_data/msg.csv").split("\n").each do |line|
   data = line.strip.split("\t")
@@ -17,12 +19,12 @@ File.read("db/seed_data/msg.csv").split("\n").each do |line|
   )
 end
 
-puts "Inserting en_message data.."
-File.read("db/seed_data/msg_en.csv").split("\n").each do |line|
-  data = line.strip.split("\t")
-  Message.create(
-      id: data[0],
-      name: data[1],
-      lang: 1
-  )
-end
+# puts "Inserting en_message data.."
+# File.read("db/seed_data/msg_en.csv").split("\n").each do |line|
+#   data = line.strip.split("\t")
+#   Message.create(
+#       id: data[0],
+#       name: data[1],
+#       lang: 1
+#   )
+# end
